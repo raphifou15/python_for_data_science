2ae50c7fcb3173e964cd6d0a363b4dd4ab099315
rkhelif <rkhelif@student.42.fr>
2024-08-05 16:50:02 +0200

    change Readme

--- starting/ex00/Hello.py
+++ starting/ex00/Hello.py
@@ -1,7 +1,7 @@
 ft_list = ["Hello", "tata!"]
 ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
-ft_dict = {"Hello" : "titi!"}
+ft_dict = {"Hello": "titi!"}
 
 ft_list[1] = "World!"
 ft_tuple = ("Hello", "France!")
